ff4562fa9f4b7bb4477034043c3eabd2639e1f56
kpvaishnani <khushbup5511@gmail.com>
2024-10-23 10:11:07 +0530

    added new components

---
+++ hushtable/src/app/home/menu/menu-items-list/menu-items-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-items-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-items-list.component.html',
+  styleUrl: './menu-items-list.component.scss'
+})
+export class MenuItemsListComponent {
+
+}
