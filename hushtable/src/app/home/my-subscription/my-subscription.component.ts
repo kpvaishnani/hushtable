566644f570efbbb69620da576a0d9e65bbf5b230
kpvaishnani <khushbup5511@gmail.com>
2024-10-24 12:22:22 +0530

    added subscription ui

--- hushtable/src/app/home/my-subscription/my-subscription.component.ts
+++ hushtable/src/app/home/my-subscription/my-subscription.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-my-subscription',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './my-subscription.component.html',
   styleUrl: './my-subscription.component.scss'
 })
