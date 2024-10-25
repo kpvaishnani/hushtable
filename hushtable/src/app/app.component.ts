f8c455e1494e3dfa715fc47a530f804203268343
kpvaishnani <khushbup5511@gmail.com>
2024-10-25 13:14:07 +0530

    firebase hosting init

--- hushtable/src/app/app.component.ts
+++ hushtable/src/app/app.component.ts
@@ -15,15 +15,7 @@ import { initializeApp } from 'firebase/app';
 export class AppComponent {
   title = 'hushtable';
 
-  firebaseConfig = {
-    apiKey: "AIzaSyDuDlQWsWlkGiEmh73NYsOAT4qB4F6qAY0",
-    authDomain: "hushtable-157a2.firebaseapp.com",
-    projectId: "hushtable-157a2",
-    storageBucket: "hushtable-157a2.appspot.com",
-    messagingSenderId: "356603333565",
-    appId: "1:356603333565:web:16b2303230b979be05e0d4",
-    measurementId: "G-G51Z3J3HN5"
-  };
 
-  app = initializeApp(this.firebaseConfig);
+
+ 
 }
--- hushtable/src/app/app.config.ts
+++ hushtable/src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuDlQWsWlkGiEmh73NYsOAT4qB4F6qAY0",
+  authDomain: "hushtable-157a2.firebaseapp.com",
+  projectId: "hushtable-157a2",
+  storageBucket: "hushtable-157a2.appspot.com",
+  messagingSenderId: "356603333565",
+  appId: "1:356603333565:web:16b2303230b979be05e0d4",
+  measurementId: "G-G51Z3J3HN5"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: 
@@ -10,6 +23,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), provideClientHydration(), 
     provideAnimationsAsync(),
-   
+    importProvidersFrom(
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireAuthModule
+    ),
   ]
 };
