0ca785e8be43e96741e8493df0bc99d52fd31eb0
kpvaishnani <khushbup5511@gmail.com>
2024-10-25 12:01:51 +0530

    added firebase config

--- hushtable/src/app/app.component.ts
+++ hushtable/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from 'firebase/app';
 
 
 
@@ -13,4 +14,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'hushtable';
+
+  firebaseConfig = {
+    apiKey: "AIzaSyDuDlQWsWlkGiEmh73NYsOAT4qB4F6qAY0",
+    authDomain: "hushtable-157a2.firebaseapp.com",
+    projectId: "hushtable-157a2",
+    storageBucket: "hushtable-157a2.appspot.com",
+    messagingSenderId: "356603333565",
+    appId: "1:356603333565:web:16b2303230b979be05e0d4",
+    measurementId: "G-G51Z3J3HN5"
+  };
+
+  app = initializeApp(this.firebaseConfig);
 }
--- hushtable/src/app/app.config.ts
+++ hushtable/src/app/app.config.ts
@@ -1,13 +1,8 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-
-import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: 
@@ -15,9 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), provideClientHydration(), 
     provideAnimationsAsync(),
-    importProvidersFrom(
-      AngularFireModule.initializeApp(environment.firebase),
-      AngularFireAuthModule
-    ),
+   
   ]
 };
